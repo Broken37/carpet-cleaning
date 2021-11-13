a44a2aceb0614fe8c7dc5de13aa2fade162acafb
m.dehghankar <mohsendehghankar@gmail.com>
2021-11-13 20:14:17 +0330

    gitignore updated, User model added

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from users.models import User
+
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "id", "phone_number")
+    list_display_links = ("id", "phone_number", "last_name")
+    search_fields = ("phone_number", "first_name", "last_name")
+
+
+admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.core.validators import RegexValidator
+
 
 # Create your models here.
+class User(AbstractUser):
+    phone_regex = RegexValidator(
+        regex=r"^(09)\d{9}$",
+        message="phone_number format: 09xxxxxxxxx",
+    )
+    phone_number = models.CharField(
+        validators=[phone_regex], max_length=11, null=False, blank=False, unique=True
+    )
