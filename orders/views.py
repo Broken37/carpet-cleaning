38a57900994b70e208065b2e6be054e37ff490ed
m.dehghankar <mohsendehghankar@gmail.com>
2021-11-13 22:50:33 +0330

    templates added with an example

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+def example_view(request):
+    template = loader.get_template("example.html")
+    context = {
+        "message": "this is message",
+    }
+    return HttpResponse(template.render(context, request))
