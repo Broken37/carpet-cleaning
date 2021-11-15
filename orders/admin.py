764f0931ae0d37388e0fdda4b36ea8d34b21d890
m.dehghankar <mohsendehghankar@gmail.com>
2021-11-15 22:27:12 +0330

    order list permissions added based on owners + django admin groups fixed

--- orders/admin.py
+++ orders/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
 
 from orders.models import Order
+from users.models import CarpetCleaning, UserType
 
-# Register your models here.
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ("id", "customer", "carpet_cleaning", "status")
-    list_display_links = ("id", )
-    list_filter = ("status", )
+    list_display_links = ("id",)
+    list_filter = ("status",)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        if not self.has_view_or_change_permission(request):
+            queryset = queryset.none()
+        if request.user.user_type == UserType.carpet_cleaning_owner:
+            # current user's carpet cleaning
+            carpet_cleaning = CarpetCleaning.objects.filter(owner=request.user)
+            queryset = queryset.filter(carpet_cleaning__id__in=carpet_cleaning)
+
+        return queryset
 
 
 admin.site.register(Order, OrderAdmin)
