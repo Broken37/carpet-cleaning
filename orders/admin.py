8195005b9843adefd4bb113f6b1582247362a3a3
ArmanZarei <armanzarei1378@gmail.com>
2021-11-17 13:38:20 +0330

    Approve order action

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from orders.models import Order
+from orders.models import Order, OrderStatus
 from users.models import CarpetCleaning, UserType
 
 
@@ -8,6 +8,7 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ("id", "customer", "carpet_cleaning", "status")
     list_display_links = ("id",)
     list_filter = ("status",)
+    actions = ["approve_status"]
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
@@ -19,6 +20,11 @@ class OrderAdmin(admin.ModelAdmin):
             queryset = queryset.filter(carpet_cleaning__id__in=carpet_cleaning)
 
         return queryset
+    
+    @admin.action(description=f'Mark selected orders as {OrderStatus.labels[OrderStatus.approved]}')
+    def approve_status(self, request, queryset):
+        queryset.update(status=OrderStatus.approved)
+    
 
 
 admin.site.register(Order, OrderAdmin)
