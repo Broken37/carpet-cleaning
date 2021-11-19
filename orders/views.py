b33a7cf9fa97f3f840f9f702f8387b4be0ecc474
Parham Saremi <parhaamsaremi@gmail.com>
2021-11-19 19:25:07 +0330

    removed test path from order urls

--- orders/views.py
+++ orders/views.py
@@ -5,14 +5,6 @@ from .models import Order
 from users.models import User
 from users.models import CarpetCleaning
 
-
-
-def example_view(request):
-    template = loader.get_template("orders/example.html")
-    context = {
-        "test_list": list(range(5)),
-    }
-    return HttpResponse(template.render(context, request))
 def register_page(request, carpet_cleaning_id):
     if request.method == "POST":
         number = request.POST.get("number")
